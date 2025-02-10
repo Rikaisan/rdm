48a9813775e1e707f3d349732a86a1978223e094
Rikai <rikaisensei@gmail.com>
2025-02-10 12:37:51 -0500

    feat(Logger): Don't consider Error and Warn debug logs

--- src/Logger.hpp
+++ src/Logger.hpp
@@ -8,29 +8,29 @@
 #include <iostream>
 #define LOG(x) do {std::cout << x << std::endl;} while(0)
 #define LOG_INFO(x) do {std::cout << INFO_TAG << x << std::endl;} while(0)
+#define LOG_ERR(x) do {std::cerr << ERR_TAG << x << std::endl;} while(0)
+#define LOG_WARN(x) do {std::cerr << WARN_TAG << x << std::endl;} while(0)
 #define LOG_CUSTOM(name, x) do {std::cout << "\033[94m[" << name << "]\033[m " << x << std::endl;} while(0)
 #define LOG_CUSTOM_INFO(name, x) do {std::cout << "\033[94m[" << name << "]" << INFO_TAG << x << std::endl;} while(0)
+#define LOG_CUSTOM_ERR(name, x) do {std::cerr << "\033[94m[" << name << "]" << ERR_TAG << x << std::endl;} while(0)
+#define LOG_CUSTOM_WARN(name, x) do {std::cerr << "\033[94m[" << name << "]" << WARN_TAG << x << std::endl;} while(0)
 #define LOG_SEP() do {std::cout << "+------------------------------------------------+" << std::endl;} while(0)
 #else
 #define LOG(x)
 #define LOG_INFO(x)
+#define LOG_ERR(x)
+#define LOG_WARN(x)
 #define LOG_CUSTOM(name, x)
 #define LOG_CUSTOM_INFO(name, x)
+#define LOG_CUSTOM_ERR(name, x)
+#define LOG_CUSTOM_WARN(name, x)
 #define LOG_SEP()
 #endif
 
 #if defined(_DEBUG) && ! defined(_NLOG)
-#define LOG_ERR(x) do {std::cerr << ERR_TAG << x << std::endl;} while(0)
-#define LOG_WARN(x) do {std::cerr << WARN_TAG << x << std::endl;} while(0)
 #define LOG_DEBUG(x) do {std::cerr << DEBUG_TAG << x << std::endl;} while(0)
-#define LOG_CUSTOM_ERR(name, x) do {std::cerr << "\033[94m[" << name << "]" << ERR_TAG << x << std::endl;} while(0)
-#define LOG_CUSTOM_WARN(name, x) do {std::cerr << "\033[94m[" << name << "]" << WARN_TAG << x << std::endl;} while(0)
 #define LOG_CUSTOM_DEBUG(name, x) do {std::cerr << "\033[94m[" << name << "]" << DEBUG_TAG << x << std::endl;} while(0)
 #else
-#define LOG_ERR(x)
-#define LOG_WARN(x)
 #define LOG_DEBUG(x)
-#define LOG_CUSTOM_ERR(name, x)
-#define LOG_CUSTOM_WARN(name, x)
 #define LOG_CUSTOM_DEBUG(name, x)
 #endif
